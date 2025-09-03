42e06dc9470a70365ecd9013002adf8660c00d55
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-09-03 16:53:02 +0200

    Implement DefaultLayout and HomePage components with routing setup

---
+++ fluffman_pw/front-end/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to Fluffman</h1>
+        </div>
+    );
+}
--- fluffman_pw/front-end/src/App.jsx
+++ fluffman_pw/front-end/src/App.jsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+import HomePage from "../pages/HomePage";
 
 function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DefaultLayout />}>
+            <Route index element={<HomePage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
