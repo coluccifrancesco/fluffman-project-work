7076fb92a54a91ee316419f4b28175ec1aec0f7a
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-09-04 19:36:20 +0200

    fix CardProductDetails component, refactor SingleProductPage styles and content, add SingleProductPage.css file to further style up product details page

--- fluffman_pw/front-end/components/CardComponent/CardProductDetails.jsx
+++ fluffman_pw/front-end/components/CardComponent/CardProductDetails.jsx
@@ -1,23 +1,27 @@
 export default function CardProductDetail() {
-    return (
-        <>
-            <div className="card mb-3">
-                <div className="row g-0">
-                    <div className="col-md-4">
-                        <img src="..." className="img-fluid rounded-start" alt="product_img" />
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                            <h5 className="product_name">Nome del prodotto</h5>
-                            <p className="producer">Nome dell'azienda produttrice</p>
-                            <p className="price">Prezzo del prodotto</p>
-                        </div>
-                        <div className="card_bottom">
-                            <p>Descrizione del Prodotto: Lorem ipsum dolor sit amet consectetur adipisicing elit. Et molestiae voluptatem fuga repudiandae nihil aspernatur a laborum pariatur enim. A, laboriosam libero sapiente earum quidem vitae ex reiciendis itaque ea.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <div className="card p-3 mb-3">
+      <div className="card-body text-center">
+        <h5 className="product_name text-dark">Nome del prodotto</h5>
+        <p className="producer text-dark">Nome dell'azienda produttrice</p>
+        <p className="price text-dark">Prezzo del prodotto</p>
+
+        {/* Immagine spostata sotto il titolo */}
+        <div className="card-top mx-auto my-3" style={{ width: "100%" }}>
+          <img
+            src="/happy_dog.jpg"
+            className="img-fluid rounded detail_img"
+            alt="product_img"
+          />
+        </div>
+
+        <div className="card_bottom">Lorem ipsum dolor sit amet.</div>
+        <p className="text-dark">Breve Descrizione Prodotto</p>
+        <p className="text-dark">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+          exercitationem.
+        </p>
+      </div>
+    </div>
+  );
 }
--- fluffman_pw/front-end/pages/SingleProductPage.jsx
+++ fluffman_pw/front-end/pages/SingleProductPage.jsx
@@ -1,29 +1,59 @@
 import CardProductDetail from "../components/CardComponent/CardProductDetails";
 import CardItem from "../components/CardComponent/CardItem";
+import "../styles/SingleProductPage.css";
 
 export default function SingleProductPage() {
-    return (
-        <>
-            <div className="container">
+  return (
+    <div className="bg">
+      <div className="container p-2">
+        <div className="text-center">
+          <h1 className="p-2">Dettagli sul prodotto</h1> {/* product.name */}
+        </div>
 
-                <div>
-                    <h2>Dettagli sul prodotto</h2>
-                    <div>
-                        <CardProductDetail />
-                        <button className="btn" type="button">Aggiungi al Carrello</button>
-                    </div>
-
-                    <div className="m-2 p-2">
-                        <h2>Prodotti simili</h2>
-                        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-6 g-4">
-                            {[...Array(6)].map((_, idx) => (
-                                <CardItem key={idx} title={`Scelta ${idx + 1}`} />
-                            ))}
-                        </div>
-                    </div>
-                </div>
-
+        {/* Sezione Card + Descrizione */}
+        <div className="row align-items-start g-4">
+          <div className="col-md-6">
+            <CardProductDetail />
+          </div>
+          <div className="col-md-6 d-flex flex-column justify-content-around">
+            <div className="p-3 border rounded h-100 bg-light">
+              <p className="text-dark">
+                Descrizione Lunga Prodotto Lorem ipsum dolor, sit amet
+                consectetur adipisicing elit. Adipisci voluptas fuga provident
+                et laborum sapiente nemo voluptate atque distinctio culpa libero
+                magnam, vel possimus impedit molestias totam doloremque dolorum!
+                Maiores?
+              </p>
+              <p className="text-dark">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Tempore, molestias!
+              </p>
+              <div className="tags">
+                <ul className="list-unstyled d-flex flex-row gap-3 justify-content-center">
+                  <li className="tag text-white p-2  rounded">Tag</li>
+                  <li className="tag text-white p-2  rounded">Tag</li>
+                  <li className="tag text-white p-2  rounded">Tag</li>
+                </ul>
+              </div>
             </div>
-        </>
-    )
+            <div className="d-flex justify-content-center">
+              <button className="cart-btn mt-3 w-50 p-2" type="button">
+                Aggiungi al Carrello
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* Prodotti simili */}
+        <div className="m-2 p-2">
+          <h2>Prodotti simili</h2>
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-6 g-4">
+            {[...Array(6)].map((_, idx) => (
+              <CardItem key={idx} title={`Scelta ${idx + 1}`} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
