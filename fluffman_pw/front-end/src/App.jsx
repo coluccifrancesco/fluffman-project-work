4c859fb97025cf6322010c921a1a933aa4be9880
FrancescoSls97 <frankds1997@gmail.com>
2025-09-04 14:22:21 +0200

    add dog product page with placeholders

--- fluffman_pw/front-end/src/App.jsx
+++ fluffman_pw/front-end/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DefaultLayout from "../layouts/DefaultLayout";
 import HomePage from "../pages/HomePage";
+import DogProducts from "../components/DogProducts";
 
 
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<DefaultLayout />}>
             <Route path="/" element={<HomePage />} />
+            <Route path="/DogProducts" element={<DogProducts />} />
           </Route>
         </Routes>
       </BrowserRouter>
