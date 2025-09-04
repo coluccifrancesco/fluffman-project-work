743942867c02ac01ca538a62397fd448b0401f04
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-09-04 15:36:28 +0200

    refactor: remove best sellers section from HomePage component

--- fluffman_pw/front-end/pages/HomePage.jsx
+++ fluffman_pw/front-end/pages/HomePage.jsx
@@ -40,17 +40,14 @@ export default function HomePage() {
                         </div>
                     </div>
 
-                    {/* Section -  i più venduti */}
-                    <h2 className="mb-3">I più venduti</h2>
-                    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-6 g-4">
-                        {[...Array(6)].map((_, idx) => (
-                            <CardItem key={idx} title={`Scelta ${idx + 1}`} />
-                        ))}
-                    </div>
+
 
                     {/* Section - Ultimi Arrivi */}
                     <NewProducts />
+                    <DogProducts />
+                    <CatProducts />
                 </div>
             </div>
-            );
+        </div>
+    );
 }
