c3a8507f7430543baec27fa9b81db10866236160
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-09-05 10:07:09 +0200

    feat: update database configuration and improve MySQL connection handling

--- fluffman_pw/back_end/db/connection.js
+++ fluffman_pw/back_end/db/connection.js
@@ -11,11 +11,13 @@ export default pool;
 
 // Configurazione per MySQL
 import mysql from 'mysql2/promise';
-require('dotenv').config({ path: '../.env' });
+// require('dotenv').config({ path: '../.env' });
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_DATABASE || 'your_database'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
 export default pool;
