ef11c7c74e1c78306f4a8fd94a5080457b607965
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-09-07 20:11:13 +0200

    Enhance CardItem component with navigation links and improve styling

--- fluffman_pw/front-end/components/CardComponent/CardItem.jsx
+++ fluffman_pw/front-end/components/CardComponent/CardItem.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function CardItem({ product }) {
   const navigate = useNavigate();
@@ -12,7 +13,7 @@ export default function CardItem({ product }) {
   const imageUrl = product?.image;
 
   return (
-    <div className="card h-100 product_card text-center" onClick={handleCardClick}>
+    <div className="card product_card text-center" onClick={handleCardClick}>
       <div className="card-top mx-auto my-3 card-image-container">
         {imageUrl ? (
           <img
@@ -24,9 +25,20 @@ export default function CardItem({ product }) {
           <div className="placeholder-image">Immagine non disponibile</div>
         )}
       </div>
-      <div className="card-body ">
-        <h5 className="card-title product_name">{product?.name}</h5>
-        <p className="card-text product_price">{product?.price} $</p>
+      <div className="card-body">
+        <h5 className="card-title fs-4 product_name">{product?.name}</h5>
+        <div className="card-bottom d-flex justify-content-around">
+          <p className="card-text fs-5 pt-1 product_price">{product?.price} $</p>
+          <div className="card-buttons d-flex align-items-start">
+            <NavLink to="/">
+              <i class="bi bi-star fs-4 me-3"></i>
+            </NavLink>
+            <NavLink to="/">
+              <i class="bi bi-cart3 fs-4"></i>
+            </NavLink>
+          </div>
+
+        </div>
       </div>
     </div>
   );
