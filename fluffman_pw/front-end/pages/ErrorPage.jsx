ed2bb3ae34c602663f8bd669fe1736453e597204
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-09-08 12:47:50 +0200

    Refactor ErrorPage component and add ErrPage.css styles for animation and styling

--- fluffman_pw/front-end/pages/ErrorPage.jsx
+++ fluffman_pw/front-end/pages/ErrorPage.jsx
@@ -1,19 +1,28 @@
-import "../styles/HomePage.css";
+import "../styles/ErrorPage.css";
 import { Link } from "react-router-dom";
 
 export default function Error404Page() {
-    return (
-        <div className="hp_bg d-flex align-items-center justify-content-center text-center p-5">
-            <div className="container">
-                <h1 className="display-1 fw-bold">404</h1>
-                <h2 className="mb-4">Oops! Pagina non trovata.</h2>
-                <p className="fs-5 mb-5 text-black">
-                    Sembra che la pagina che stai cercando non esista o sia stata spostata.
-                </p>
-                <Link to="/" className="btn btn-outline-danger btn-lg">
-                    Torna alla Homepage
-                </Link>
-            </div>
-        </div>
-    );
+  return (
+    <div className="error-bg">
+      <div id="clouds">
+        <div className="cloud x1"></div>
+        <div className="cloud x1_5"></div>
+        <div className="cloud x2"></div>
+        <div className="cloud x3"></div>
+        <div className="cloud x4"></div>
+        <div className="cloud x5"></div>
+      </div>
+
+      {/* Contenitore centrale con testo */}
+      <div className="c">
+        <div className="_404">404</div>
+
+        <div className="_1">THE PAGE</div>
+        <div className="_2">WAS NOT FOUND</div>
+        <Link className="btn-404-home" to="/">
+          BACK HOME
+        </Link>
+      </div>
+    </div>
+  );
 }
