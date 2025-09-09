f30a5cca6257e60b84a11a00f7baad4593437941
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-09-09 17:36:15 +0200

    Revamp ErrorPage with animated pet emojis and updated styling; replace clouds with flying pets for enhanced visual appeal

--- fluffman_pw/front-end/pages/ErrorPage.jsx
+++ fluffman_pw/front-end/pages/ErrorPage.jsx
@@ -4,19 +4,17 @@ import { Link } from "react-router-dom";
 export default function Error404Page() {
   return (
     <div className="error-bg">
-      <div id="clouds">
-        <div className="cloud x1"></div>
-        <div className="cloud x1_5"></div>
-        <div className="cloud x2"></div>
-        <div className="cloud x3"></div>
-        <div className="cloud x4"></div>
-        <div className="cloud x5"></div>
+      <div id="flying-pets">
+        {/* Le classi .pet ora usano le emoji tramite CSS */}
+        <div className="pet dog-1"></div>
+        <div className="pet cat-1"></div>
+        <div className="pet dog-2"></div>
+        <div className="pet bird-1"></div>
+        <div className="pet cat-2"></div>
       </div>
 
-      {/* Contenitore centrale con testo */}
       <div className="c">
         <div className="_404">404</div>
-
         <div className="_1">THE PAGE</div>
         <div className="_2">WAS NOT FOUND</div>
         <Link className="btn-404-home" to="/">
